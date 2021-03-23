61ce6684e141d1c9978ffa0e4b38fb5b12880461
SupreetiKumari <78208708+SupreetiKumari@users.noreply.github.com>
2021-03-23 23:58:56 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+//
+//  main.cpp
+//  face detection
+//
+//  Created by Supreeti Kumari on 22/03/21.
+//
+
+#include <opencv2/imgcodecs.hpp>
+  #include <opencv2/highgui.hpp>
+  #include <opencv2/imgproc.hpp>
+  #include <iostream>
+#include <opencv2/opencv.hpp>
+
+  using namespace cv;
+  using namespace std;
+
+
+  ///////////////// Webcam //////////////////////
+//
+//   int main() {
+//
+//   VideoCapture cap(0);
+//   Mat img;
+//
+//   while (true) {
+//
+//   cap.read(img);
+//imshow("imagee",img);
+//   waitKey(1);
+//
+//   }
+//      return 0;
+//  }
+
